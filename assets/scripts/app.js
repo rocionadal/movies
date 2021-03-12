ea0af2c3aaca4418248f508885c1dd7ab1dc25af
rocionadal <rocioonadal@gmail.com>
2021-03-12 21:47:15 +0100

    rendering movides

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,8 +4,33 @@ const backdrop = document.getElementById('backdrop');
 const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
 const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
 const userInputs = addMovieModal.querySelectorAll('input');
+const entryTextSection = document.getElementById('entry-text');
 
 const movies = [];
+
+const updateUI = () => {
+  if (movies.length === 0) {
+    entryTextSection.style.display = 'block';
+  } else {
+    entryTextSection.style.display = 'none';
+  }
+}
+
+const renderNewMovieElement = (title, imageUrl, rating) => {
+  const newMovieElement = document.createElement('li');
+  newMovieElement.className = 'movie-element';
+  newMovieElement.innerHTML = `
+  <div class="movie-element__image">
+    <img src="${imageUrl}" alt="${title}">
+  </div>
+  <div class="movie-element__info">
+    <p>${title}</p>
+    <p>${rating}/5 stars</p>
+  </div>
+  `;
+  const list = document.getElementById('movie-list');
+  list.append(newMovieElement);
+}
 
 const toggleBackdrop = () => {
   backdrop.classList.toggle('visible');
@@ -53,6 +78,8 @@ const addMovieHandler = () => {
   console.log(movies);
   toggleMovieModal();
   clearMovieInputs();
+  renderNewMovieElement(newMovie.title, newMovie.image, newMovie.rating);
+  updateUI();
 }
 
 const backdropClickHandler = () => {
